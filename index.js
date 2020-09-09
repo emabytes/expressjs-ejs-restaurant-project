d599e78a8a18564cc5539db74f0f89dad59ec115
emabytes <emadavor22@gmail.com>
2020-09-09 23:43:11 +0200

    contact form

--- index.js
+++ index.js
@@ -2,19 +2,37 @@ const express = require("express")
 const app = express()
 const nav = require("./menu.json")
 
-app.set("view engine", "ejs")
+const bodyParser = require('body-parser')
 
 app.listen(5003, () => {
     console.log("server started at http://localhost:5003")
 })
 
+var jsonParser = bodyParser.json()
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 app.use(express.static("public"))
+app.set("view engine", "ejs")
+
+// app.get("/", (res, req) => {
+//     res.status(200).render("index")
+// })
 
 nav.forEach((elem) => {
     app.get(elem.url, (req, res) => {
         res.render(elem.name.toLowerCase(), { title: elem.name, nav: nav })
     })
 });
+
+app.post('/contactForm', urlencodedParser, (req, res) => {
+    console.log(req.body)
+    console.log(req.body.name);
+    console.log(req.body.email);
+    console.log(req.body.phone);
+    console.log(req.body.message);
+
+    res.status(201).send("created")
+})
 
 app.use((req, res) => {
     res.render('404', { title: 'Not found', nav: nav })
